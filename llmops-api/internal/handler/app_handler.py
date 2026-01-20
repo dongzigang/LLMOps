16d459339147b8853896886376a2813b830c1121
dongzigang <821220992@qq.com>
2026-01-20 17:27:55 +0800

    update

--- llmops-api/internal/handler/app_handler.py
+++ llmops-api/internal/handler/app_handler.py
@@ -7,37 +7,40 @@
 """
 import os
 import uuid
-from flask import request
-from openai import OpenAI
+from dataclasses import dataclass
+
+from injector import inject
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
 from internal.exception import NotFoundException
 from internal.schema.app_schema import CompletionReq
 from internal.service import AppService
 from pkg.response import success_json, validate_error_json, success_message
-from dataclasses import dataclass
-from injector import inject
+
 
 @inject
 @dataclass
 class AppHandler:
     """" 应用控制器 """
-    app_service:AppService
+    app_service: AppService
+
     def create_app(self):
         app = self.app_service.create_app()
         return success_message(f"应用创建成功，id={app.id}")
 
-    def get_app(self, id:uuid.UUID):
+    def get_app(self, id: uuid.UUID):
         app = self.app_service.get_app(id)
         return success_message(f"应用成功获取，name={app.name}")
 
-    def update_app(self, id:uuid.UUID):
+    def update_app(self, id: uuid.UUID):
         app = self.app_service.update_app(id)
         return success_message(f"应用成功修改，name={app.name}")
 
-    def delete_app(self, id:uuid.UUID):
+    def delete_app(self, id: uuid.UUID):
         app = self.app_service.delete_app(id)
         return success_message(f"应用成功删除，name={app.name}")
-
 
     def ping(self):
         raise NotFoundException('no data')
@@ -48,15 +51,17 @@ class AppHandler:
         if not req.validate():
             return validate_error_json(req.errors)
 
-        query = request.json.get("query")
-        client = OpenAI(api_key=os.getenv("DASHSCOPE_API_KEY"), base_url=os.getenv("DASHSCOPE_API_BASE"))
-        completion = client.chat.completions.create(
+        prompt = ChatPromptTemplate.from_template("{query}")
+
+        llm = ChatOpenAI(
+            api_key=os.getenv("DASHSCOPE_API_KEY"),
+            base_url=os.getenv("DASHSCOPE_API_BASE"),
             model="qwen-plus",
-            messages=[
-                {"role": "system", "content": "你是一个股票专家，请根据用户输入回复"},
-                {"role": "user", "content": query}
-            ],
         )
-        content = completion.choices[0].message.content
+
+        parser = StrOutputParser()
+
+        chain = prompt | llm | parser
+        content = chain.invoke({"query": req.query.data})
 
         return success_json(data={"content": content})
--- llmops-api/test.py
+++ llmops-api/test.py
@@ -5,7 +5,36 @@
 @Author: 199312306017deg@gmail.com
 @File:   test
 """
+import os
+from datetime import datetime
 
-from langchain_core.prompts import PromptTemplate
+import dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
-prompt = PromptTemplate.from_template("")
+dotenv.load_dotenv()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "你是阿里千问开发的机器人，请回答用户提出的问题，现在的时间是{now}"),
+    ("human", "{query}")
+]).partial(now=datetime.now())
+
+chatLLM = ChatOpenAI(
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url=os.getenv("DASHSCOPE_API_BASE"),
+    model="qwen-plus",
+)
+
+print("请输入内容（输入 '结束' 或 'exit' 退出）：")
+print("-" * 30)
+
+while True:
+    user_input = input("> ")
+
+    if user_input.lower() in ['结束', 'exit', 'quit', 'q']:
+        print("输入结束！")
+        break
+    response = chatLLM.stream(prompt.invoke({"query": user_input}))
+
+    for chunk in response:
+        print(chunk.content, flush=True, end='')
